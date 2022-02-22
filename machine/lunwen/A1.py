92f00ff116d482854540d67ca692c8fef2bec04c
yangsong <ys10194@163.com>
2022-02-22 17:10:39 +0800

    start

--- machine/lunwen/A1.py
+++ machine/lunwen/A1.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data = pd.read_csv('F:/个人/学校/学习资料/UNSW-NB15 - CSV Files/UNSW-NB15_4.csv')
+data = pd.read_csv('F:/个人/学校/学习资料/UNSW-NB15 - CSV Files/a part of training and testing set/UNSW_NB15_training-set.csv')
+plt.figure((figsize=(10, 16)))
 sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2) #data.corr()-->correlation matrix
 fig=plt.gcf()
-fig.set_size_inches(500,400)
+fig.set_size_inches(10000,8000)
 plt.show()
