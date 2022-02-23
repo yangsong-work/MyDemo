db5fe42f955335b65b55b6bff038798dbc024543
yangsong <ys10194@163.com>
2022-02-23 17:33:10 +0800

    start

--- machine/lunwen/A1.py
+++ machine/lunwen/A1.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-data = pd.read_csv('F:/个人/学校/学习资料/UNSW-NB15 - CSV Files/a part of training and testing set/UNSW_NB15_training-set.csv')
-plt.figure((figsize=(10, 16)))
-sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2) #data.corr()-->correlation matrix
+data = pd.read_csv('D:/Users/yangsong/PycharmProjects/pythonProject2/machine/lunwen/UNSW_NB15_training-set.csv')
+# plt.figure((figsize=(10, 16)))
+sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.1,annot_kws={'size':5}) #data.corr()-->correlation matrix
 fig=plt.gcf()
 fig.set_size_inches(10000,8000)
 plt.show()
---
+++ machine/lunwen/study.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 读取数据
+data = pd.read_csv('D:/Users/yangsong/PycharmProjects/pythonProject2/machine/lunwen/UNSW_NB15_training-set.csv')
+# 查看数据结构
+# 查看数据缺失值
+print(data.isnull().sum())
+
+# 查看被攻击的比例
+f,ax=plt.subplots(1,2,figsize=(18,8))
+data['label'].value_counts().plot.pie(explode=[0,0.1],autopct='%1.1f%%',ax=ax[0],shadow=True)
+ax[0].set_title('label')
+ax[0].set_ylabel('')
+sns.countplot('label',data=data,ax=ax[1])
+ax[1].set_title('label')
+plt.show()
+
+
+
+# 特征预处理
+
